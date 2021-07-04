58c8ac93038b2f0de0925180ea1a87bb1640d3e8
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-07-03 23:48:47 -0400

    Add 2 way binding

--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -1,12 +1,25 @@
 import { Component } from "@angular/core";
 
 
-@Component ({
+@Component({
     selector: 'app-warning',
     templateUrl: './warning.component.html',
-styleUrls: ['./warning.component.css']
+    styleUrls: ['./warning.component.css']
 })
 
 export class WarningComponent {
+
+    userName: string;
+    userStatus = 'User not created';
+
+    addUser(event: any): any {
+
+        this.userName = event.target.value;
+
+    }
+
+    userCreated(): any {
+        this.userStatus = 'User: ' + this.userName + ' created';
+    }
 
 }
