3aa147aa22ff972ff8f0409cd8d4ab8076c83fa9
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-07-07 23:08:12 -0400

    Add test component

---
+++ src/app/testserver/testserver.component.ts
@@ -0,0 +1,14 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-testserver',
+    templateUrl: './testserver.component.html',
+    styleUrls: ['./testserver.component.css']
+})
+
+export class TestserverComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
