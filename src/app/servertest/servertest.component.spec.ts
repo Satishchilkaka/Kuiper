906461fe8d4a190f6ee932ba3fae9fb5d0db21a6
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-07-12 22:01:49 -0400

    Add test server component

---
+++ src/app/servertest/servertest.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServertestComponent } from './servertest.component';
+
+describe('ServertestComponent', () => {
+  let component: ServertestComponent;
+  let fixture: ComponentFixture<ServertestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ServertestComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ServertestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
