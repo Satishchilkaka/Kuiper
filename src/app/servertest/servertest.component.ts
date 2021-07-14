d77ead659b2d4acec63a9c2015005375678a3cd9
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-07-13 22:32:04 -0400

    ngstyle

--- src/app/servertest/servertest.component.ts
+++ src/app/servertest/servertest.component.ts
@@ -7,15 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServertestComponent implements OnInit {
 
-  constructor() { }
   userInput: string;
   serverName: string;
+  serverId = 100;
+  serverStatus = 'Offline';
+  constructor() {
+    this.serverStatus = Math.random() * 10 > 3 ? 'Online' : 'Offline';
+    console.log(Math.random() * 7);
+  }
+  getServerStatus(): any {
+    return this.serverStatus;
+  }
 
   enteredServerName(event: any): any {
     this.userInput = event.target.value;
   }
   addTestServerName(): any {
     this.serverName = this.userInput;
+  }
+  getColor(): any {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
   }
 
   ngOnInit(): void {
