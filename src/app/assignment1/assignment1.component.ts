41e095cd34b2ef9618b10a9713f2ae1c0ef6bf46
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-07-15 22:34:09 -0400

    add assignment1

--- src/app/assignment1/assignment1.component.ts
+++ src/app/assignment1/assignment1.component.ts
@@ -9,17 +9,25 @@ import * as dayjs from 'dayjs';
 })
 export class Assignment1Component implements OnInit {
 
-  users = ['user1', 'user2', 'user3'];
   constructor() { }
+  users = ['user1', 'user2', 'user3'];
+  today = new Date();
+  toggle = true;
   password: string;
+  text = 'This is text';
+  count = 0;
   details(): any {
-    this.password = 'This is new password';
+    this.toggle = !this.toggle;
+    return this.count++;
 
   }
   dateTime(): any {
     return dayjs();
-
   }
+  getColor(): any {
+    return this.count >= 5 ? 'red' : 'green';
+  }
+
   ngOnInit(): void {
   }
 
